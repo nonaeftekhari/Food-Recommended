abdaa30b55df7d35fa30b8352fb6059a5cc3b420
Nadia Chowdhury <fx5623@wayne.edu>
2019-11-07 18:20:04 -0500

    Implemented Composite pattern

---
+++ MenuComponent.java
@@ -0,0 +1,25 @@
+public abstract class MenuComponent {
+
+    public void add(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void remove(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getName() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getType() {
+        throw new UnsupportedOperationException();
+    }
+
+    public double getPrice() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String show() {
+        throw new UnsupportedOperationException();
+    }
