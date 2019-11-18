5660fedb3e7e0205bc6456afcfac40fefffe361d
ga0451 <55364247+ga0451@users.noreply.github.com>
2019-11-17 20:15:31 -0500

    Updating urls.py like Max asked.

--- FoodAPI/AppAPI/urls.py
+++ FoodAPI/AppAPI/urls.py
@@ -2,13 +2,6 @@ from django.urls import path, include
 from . import views
 from rest_framework import routers
 
-"""
-router = routers.DefaultRouter()
-router.register('Rest', views.restView)
-router.register('Item', views.itemView)
-router.register('User', views.userView)
-router.register('Rating', views.ratingView)
-"""
 
 #all custom url endpoints
 urlpatterns = [
