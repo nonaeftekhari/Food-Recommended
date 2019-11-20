9e3443dc61e24242360e6503457a24e212dba632
Max Hendricks <go4811@wayne.edu>
2019-11-19 21:13:07 -0500

    Fixed errors in splash screen

--- HomeActivity.java
+++ HomeActivity.java
@@ -1,11 +1,9 @@
 package com.example.foodrecommended_asd;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
-
 import gr.net.maroulis.library.EasySplashScreen;
 
 public class HomeActivity extends AppCompatActivity {
@@ -14,7 +12,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
+ 	// These are the required configs for the method of EasySplashScreen
         EasySplashScreen config = new EasySplashScreen(HomeActivity.this)
                 .withFullScreen()
                 .withTargetActivity(MainActivity.class)
@@ -25,7 +23,7 @@ public class HomeActivity extends AppCompatActivity {
                 .withBeforeLogoText("")
                 .withAfterLogoText("Food Recommended")
                 .withLogo(R.mipmap.ic_launcher_img_foreground);
-
+	// These are getter functions to present the text and logo
         config.getHeaderTextView().setTextColor(Color.WHITE);
         config.getFooterTextView().setTextColor(Color.WHITE);
         config.getBeforeLogoTextView().setTextColor(Color.WHITE);
