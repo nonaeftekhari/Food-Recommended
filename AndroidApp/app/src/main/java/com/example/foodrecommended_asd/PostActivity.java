f0b3f7aa233a63dbcac4e95d68073c04d0ff6f66
Collin Heck <ga0451@wayne.edu>
2019-11-21 18:42:03 -0500

    adding java

--- AndroidApp/app/src/main/java/com/example/foodrecommended_asd/PostActivity.java
+++ AndroidApp/app/src/main/java/com/example/foodrecommended_asd/PostActivity.java
@@ -1,16 +1,13 @@
 package com.example.foodrecommended_asd;
-
-
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
-import java.util.List;
+import java.util.*;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -18,83 +15,117 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+/*
+This is the class handles grabbing the restaurant (/Rest) data from the database. This class can be
+used as a template for creating classes that grab data from other parts of the database. However,
+a Class for the objects made from the json and an API will have to be created. Both class (Rest) and
+the interface (RestaurantAPI) can be refferenced templates to acheive this.
+
+NOTE: Even though the browser URL for the database is 127.0.0.1, android studio cannot access the
+database from this address, instead use 10.0.2.2 as the base URL.
+
+REQUIRED DEPENDENCIES FOR build.gradle
+    implementation 'com.squareup.retrofit2:retrofit:2.3.0'
+    implementation 'com.squareup.retrofit2:converter-gson:2.3.0'
+*/
+
+
 public class PostActivity extends AppCompatActivity {
 
+    //ifFail is a text variable that displays an error message if the data base isn't accessed and
+    //alerts why. Consider changing the method of displaying this error message to alerts with
+    //javascript.
+    //TextView ifFail = findViewById(R.id.text_view_result);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        RestaurantAPI restaurantAPI;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://127.0.0.1:8000/")
+                .baseUrl("http://10.0.2.2:8000/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        //initalizing the RetrofitAPI that read converts the json file into java variables.
+        restaurantAPI = retrofit.create(RestaurantAPI.class);
 
-        Call<List<Rest>> call = jsonPlaceHolderApi.getPosts();
+        List<Rest> restVect = new ArrayList<Rest>();
+        getPosts(restaurantAPI);
+    }
+
+    public void getPosts(RestaurantAPI restaurantAPI) {
+        Call<List<Rest>> call = restaurantAPI.getPosts();
 
         call.enqueue(new Callback<List<Rest>>() {
             @Override
             public void onResponse(Call<List<Rest>> call, Response<List<Rest>> response) {
-                if(!response.isSuccessful()) {
-                    //handle response data
-                    //textViewResult.setText("Code: " + response.code());
+
+                //Error block if the database is accessed, but runs into a problem once accessed
+                if (!response.isSuccessful()) {
+                    //ifFail.setText("code: " + response.code());
                     return;
                 }
 
+
                 List<Rest> rests = response.body();
 
-                /*
-                for(Rest rest : rests) {
-                    String content="";
-                    content += "ID: " + rest.getId() + "\n";
-                    content += "Restaurant Name: " + rest.getRestName() + "\n";
-                    content += rest.getBanner();
-                    //Bitmap banner = BitmapFactory.decodeFile(rest.getBanner());
-                    //content += banner + "\n";
-                    content += "Zipcode: " + rest.getZipcode() + "\n";
-                    content += "Website: " + rest.getWebsite() + "\n";
-                    content += "Phone number: " + rest.getPhone() + "\n";
-                    content += "Avg Price: " + rest.getRestPrice() + "\n\n";
-                    //can call this for a block od data
-                    //textViewResult.append(content);
+
+                //This for loop runs though the entire json file and saves the elements into as varibles
+                //of a rest object. These rest objects are being stored into a vector that can be
+                //accessed later.
+                for (Rest rest : rests) {
+                    restVector.add(new Rest(rest.getId(), rest.getRestName(), rest.getBanner(),
+                            rest.getZipcode(), rest.getRestRating(), rest.getWebsite(),
+                            rest.getPhone(), rest.getRestPrice(), rest.getCategory()));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<Rest>> call, Throwable t,) {
+                //ifFail.setText(t.getMessage());
+            }
+        });
+    }
+
+    public void getRestZipCat(RestaurantAPI restaurantAPI, int zip, int cat) {
+
+        Call<List<Rest>> call = restaurantAPI.getRestZipCat(zip, cat);
+
+        call.enqueue(new Callback<List<Rest>>() {
+            @Override
+            public void onResponse(Call<List<Rest>> call, Response<List<Rest>> response) {
+
+                //Error block if the database is accessed, but runs into a problem once accessed
+                if (!response.isSuccessful()) {
+                    //ifFail.setText("code: " + response.code());
+                    return;
+                }
+
+
+                List<Rest> rests = response.body();
+                Vector<Rest> restVector = new Vector<Rest>();
+
+                //This for loop runs though the entire json file and saves the elements into as varibles
+                //of a rest object. These rest objects are being stored into a vector that can be
+                //accessed later.
+                for (Rest rest : rests) {
+                    restVector.add(new Rest(rest.getId(), rest.getRestName(), rest.getBanner(),
+                            rest.getZipcode(), rest.getRestRating(), rest.getWebsite(),
+                            rest.getPhone(), rest.getRestPrice(), rest.getCategory()));
 
 
                 }
-                */
+
+
             }
 
             @Override
             public void onFailure(Call<List<Rest>> call, Throwable t) {
-                //handle message failure
-                //textViewResult.setText(t.getMessage());
+                //ifFail.setText(t.getMessage());
             }
+
+
         });
     }
-    /*
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-
-    }
-
-     */
 }
--- AndroidApp/app/src/main/java/com/example/foodrecommended_asd/Rest.java
+++ AndroidApp/app/src/main/java/com/example/foodrecommended_asd/Rest.java
@@ -1,17 +1,31 @@
 package com.example.foodrecommended_asd;
 
-
-import android.graphics.Bitmap;
-
+//Class rest that saves intializes all the varibables of a restaurants data that is stored in database
 public class Rest {
-    private int id;
-    private String restName;
-    private String banner;
-    private int zipcode;
-    private double restRating;
-    private String website;
-    private int phone;
-    private double restPrice;
+    private int id = 0;
+    private String restName = "";
+    private String banner = "";
+    private long zipcode = 0;
+    private double restRating = 0.0;
+    private String website = "";
+    private long phone = 0;
+    private double restPrice = 0.0;
+    private int category = 0;
+
+    public Rest(){}
+
+    public Rest(int id, String restName, String banner, long zipcode, double restRating,
+    String website, long phone, double restPrice, int category) {
+        this.id = id;
+        this.restName = restName;
+        this.banner = banner;
+        this.zipcode = zipcode;
+        this.restRating = restRating;
+        this.website = website;
+        this.phone = phone;
+        this.restPrice = restPrice;
+        this.category = category;
+    }
 
     public int getId() {
         return id;
@@ -25,7 +39,7 @@ public class Rest {
         return banner;
     }
 
-    public int getZipcode() {
+    public long getZipcode() {
         return zipcode;
     }
 
@@ -37,11 +51,49 @@ public class Rest {
         return website;
     }
 
-    public int getPhone() {
+    public long getPhone() {
         return phone;
     }
 
     public double getRestPrice() {
         return restPrice;
+    }
+
+    public int getCategory() { return category; }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setRestName(String restName) {
+        this.restName = restName;
+    }
+
+    public void setBanner(String banner) {
+        this.banner = banner;
+    }
+
+    public void setZipcode(long zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public void setRestRating(double restRating) {
+        this.restRating = restRating;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public void setPhone(long phone) {
+        this.phone = phone;
+    }
+
+    public void setRestPrice(double restPrice) {
+        this.restPrice = restPrice;
+    }
+
+    public void setCategory(int category) {
+        this.category = category;
     }
 }
