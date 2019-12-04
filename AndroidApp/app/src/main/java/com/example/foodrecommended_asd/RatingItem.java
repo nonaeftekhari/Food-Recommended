ce1f1475b7f13133e226912a23df7316106623ad
Collin Heck <ga0451@wayne.edu>
2019-12-03 23:04:52 -0500

    cleaning branch for merge

--- AndroidApp/app/src/main/java/com/example/foodrecommended_asd/RatingItem.java
+++ AndroidApp/app/src/main/java/com/example/foodrecommended_asd/RatingItem.java
@@ -1,4 +1,43 @@
 package com.example.foodrecommended_asd;
 
 public class RatingItem {
+
+    private int id;
+    private String userEmail;
+    private int restId;
+    private int itemId;
+    private String review;
+    private int rating;
+
+    public RatingItem(String userEmail, int restId, int itemId, String review, int rating) {
+        this.userEmail = userEmail;
+        this.restId = restId;
+        this.itemId = itemId;
+        this.review = review;
+        this.rating = rating;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public int getRestId() {
+        return restId;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public String getReview() {
+        return review;
+    }
+
+    public int getRating() {
+        return rating;
+    }
 }
